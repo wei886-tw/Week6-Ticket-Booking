e0645db6fd375d85672fe28c33089b417eca7390
W3i <lunhowl@gmail.com>
2023-08-30 13:37:31 +0800

    marquee test

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './assets/js/all.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./assets/js/all.js";
 
 console.log("Hello world!");
+
+
+const swiper = new Swiper(".mySwiper", {
+    allowTouchMove: false,
+    slidesPerView: "auto",
+    speed: 20000,
+    loop: true,
+    autoplay: {
+        delay: 0,
+    },
+});
+
